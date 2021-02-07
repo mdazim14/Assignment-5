0087e7612afa460ce9e3ef47cc1a1cf06f86c4dc
Md Azim <mdazim.cse@gmail.com>
2021-02-08 05:02:45 +0600

    again added

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 const searchMeal = () => {
     const searchText = document.getElementById('search-field').value;
-    const url = `https://www.themealdb.com/api/json/v1/1/categories.php`;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     // // load data
     fetch(url)
     .then (res => res.json())
-    .then( data =>  displayMeals(data.categories))
+    .then( data =>  displayMeals(data.meals))
 }
-
+// searchMeal();
 const displayMeals = meals => {
+    console.log(meals);
     const mealContainer = document.getElementById('meal-container');
     mealContainer.innerHTML = '';
     meals.forEach(meal => {
         const mealDiv = document.createElement('div');
+        console.log(meal);
         mealDiv.className = 'single-result row align-items-center my-3 p-3';
         mealDiv.innerHTML = `
         <div class="col-md-9">
-                <h3 class="lyrics-name">${meal.strCategory}</h3>
-            </div>
-            <div class="col-md-3 text-md-right text-center">
+
+        <div id="imgBox">
+        <img src="${meal.strMealThumb}">
+        </div>
+            <h3 class="meal-name">${meal.strMeal}</h3>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
                 <button onclick="getLyric()" class="btn btn-success">Get Order</button>
-            </div>
+        </div>
         `;
         mealContainer.appendChild(mealDiv);
+        
     })
-
+   
+}
 //     const songContainer = document.getElementById('song-container');
 //     // songContainer.innerHTML = '';
 //     
@@ -42,8 +50,6 @@ const displayMeals = meals => {
 //             </div>
 //         `;
 //         songContainer.appendChild(songDiv);
-//     })
-}
 
 // const getLyric = async (artist, title) =>{
 //     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
